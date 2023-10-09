0e439b34d4b5e2747cc1328c6a7aeb6bd84ad50c
VPolkovnikova <147090454+VPolkovnikova@users.noreply.github.com>
2023-10-09 12:41:26 +0300

    Додано лабораторну роботу №1

---
+++ LaboratoryWork1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+
+var input = File.ReadAllText("INPUT.TXT");
+if (!(
+    int.TryParse(input, out var numberCenters)
+    && numberCenters > 0
+))
+{
+    OutputWrite("Вхідний файл має містити натуральне число.");
+    return;
+}
+var result = BigInteger.Pow(3, numberCenters * (numberCenters - 1) / 2);
+OutputWrite(result.ToString());
+
+
+static void OutputWrite(string content) => File.WriteAllText("OUTPUT.TXT", content);
